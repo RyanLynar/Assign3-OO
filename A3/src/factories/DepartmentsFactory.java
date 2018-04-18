316a19f70be81645629da9edfa3b26986c3b8c9a
Adam Andrews <andr0272@algonquinlive.com>
2018-04-18 19:19:49 -0400

    Revert "Revert "Merge branch 'BetCuh'""
    This reverts commit 145f3f5639c3a65b8a8639012f8629b4803b1640.

---
+++ A3/src/factories/DepartmentsFactory.java
@@ -0,0 +1,11 @@
+package factories;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Map;
+import builders.Depart;
+import transferobj.Departments;
+
+public class DepartmentsFactory extends AbstractFactory<Departments> {
+	
+}
---
+++ A3/src/factories/DeptManagerFactory.java
@@ -0,0 +1,12 @@
+package factories;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Map;
+import builders.Depart;
+import transferobj.Departments;
+
+public class DeptManagerFactory extends AbstractFactory<DeptManager>{
+	
+	
+}
