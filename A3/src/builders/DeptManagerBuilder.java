65811444f6ba1ff14555d781a3df0e7d5e106170
Adam Andrews <andr0272@algonquinlive.com>
2018-04-19 01:17:29 -0400

    DepartmentManager classes done

--- A3/src/builders/DeptManagerBuilder.java
+++ A3/src/builders/DeptManagerBuilder.java
@@ -1,5 +1,69 @@
 package builders;
 
-public class DeptManagerBuilder {
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.ArrayList;
+
+import databaseaccess.DAODeptManager;
+import databaseaccess.DAOEmployee;
+import transferobj.DeptManager;
+import transferobj.Employee;
+
+public class DeptManagerBuilder implements AbstractBuilder<DeptManager>{
+	private ArrayList<DeptManager> eList;
+	
+	public void setString(DeptManager newDM, String data, String cName) {
+		newDM.setDeptNumber(data);
+	}
+	
+	public void setInt(DeptManager newDM, int data, String cName) {
+		newDM.setEmployeeNumber(data);
+	}
+	
+	public void setDate(DeptManager newDM, Date data, String cName) {
+		if (cName.equals("from_date")) {
+			newDM.setFromDate(data);
+		} else if (cName.equals("to_date")) {
+			newDM.setToDate(data);		}
+
+	}
+
+	@Override
+	public void build(ResultSet r) {
+		eList.clear();
+		try {
+			r.first();
+			while(!r.isAfterLast()) {
+				DeptManager entry = new DeptManager();
+				for(int i = 0;i < r.getMetaData().getColumnCount(); i++) {
+					if (r.getMetaData().getColumnType(i + 1) == Types.DATE) {
+						if (DAODeptManager.COLUMNS[i].equals(r.getMetaData().getColumnName(i + 1))) {
+							this.setDate((DeptManager) entry, r.getDate(i + 1), r.getMetaData().getColumnName(i + 1));
+						}
+						
+					} else if (r.getMetaData().getColumnType(i + 1) == Types.CHAR) {
+						if (DAODeptManager.COLUMNS[i].equals(r.getMetaData().getColumnName(i + 1))) {
+							this.setString((DeptManager) entry, r.getString(i + 1), r.getMetaData().getColumnName(i + 1));
+						}
+
+					} else if (r.getMetaData().getColumnType(i + 1) == Types.INTEGER) {
+						if (DAODeptManager.COLUMNS[i].equals(r.getMetaData().getColumnName(i + 1))) {
+							this.setInt((DeptManager) entry, r.getInt(i + 1), r.getMetaData().getColumnName(i + 1));
+						}
+					}
+				}
+			}
+
+		}catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	@Override
+	public ArrayList<DeptManager> returnList() {
+		return eList;
+	}
 
 }
--- A3/src/factories/DeptManagerFactory.java
+++ A3/src/factories/DeptManagerFactory.java
@@ -1,5 +1,34 @@
 package factories;
 
-public class DeptManagerFactory {
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Map;
+import builders.DeptManagerBuilder;
+import transferobj.DeptManager;
+
+public class DeptManagerFactory extends AbstractFactory<DeptManager>{
+	DeptManagerBuilder builder;
+	
+	public DeptManagerFactory() {
+		builder = new DeptManagerBuilder();
+	}
+
+	@Override
+	public DeptManager createFromResults(ResultSet rs) {
+		builder.build(rs);
+		return builder.returnList().get(0);
+	}
+
+	@Override
+	public DeptManager createFromMap(Map<String, String[]> map) {
+		DeptManager result = new DeptManager();
+		return null;
+	}
+
+	@Override
+	public ArrayList<DeptManager> createListFromResults(ResultSet r) {
+		builder.build(r);
+		return builder.returnList();
+	}
 
 }
